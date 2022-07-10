8db460c5ee5e0a7290112ea8580e900387cde59b
VGLoic <vincent.genod.loic@gmail.com>
2022-07-10 16:22:25 +0200

    feat: do not have task at install

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,48 +14,48 @@ dotenv.config();
 const SEMAPHORE_GOERLI_ADDRESS = "0x99aAb52e60f40AAC0BFE53e003De847bBDbC9611";
 const GROUP_ID = 123456789
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("createGroup", "Create a group on Semaphore", async (_, hre) => {
-  const [deployer] = await hre.ethers.getSigners()
+if (process.env.SKIP_LOAD) {
+  task("createGroup", "Create a group on Semaphore", async (_, hre) => {
+    const [deployer] = await hre.ethers.getSigners()
 
-  const { ISemaphore__factory } = await import("./typechain");
+    const { ISemaphore__factory } = await import("./typechain");
 
-  const semaphore = ISemaphore__factory.connect(
-    SEMAPHORE_GOERLI_ADDRESS,
-    deployer
-  )
+    const semaphore = ISemaphore__factory.connect(
+      SEMAPHORE_GOERLI_ADDRESS,
+      deployer
+    )
 
-  const tx = await semaphore.createGroup(
-    GROUP_ID,
-    20,
-    0,
-    deployer.address
-  );
-  console.log("Create group tx submitted: ", tx.hash);
+    const tx = await semaphore.createGroup(
+      GROUP_ID,
+      20,
+      0,
+      deployer.address
+    );
+    console.log("Create group tx submitted: ", tx.hash);
 
-  await tx.wait();
-  console.log("Group created");
-});
+    await tx.wait();
+    console.log("Group created");
+  });
 
-task("updateGroupAdmin", "Update group admin on Semaphore", async (_, hre) => {
-  const [deployer] = await hre.ethers.getSigners()
+  task("updateGroupAdmin", "Update group admin on Semaphore", async (_, hre) => {
+    const [deployer] = await hre.ethers.getSigners()
 
-  const { ISemaphore__factory } = await import("./typechain");
+    const { ISemaphore__factory } = await import("./typechain");
 
-  const semaphore = ISemaphore__factory.connect(
-    SEMAPHORE_GOERLI_ADDRESS,
-    deployer
-  );
+    const semaphore = ISemaphore__factory.connect(
+      SEMAPHORE_GOERLI_ADDRESS,
+      deployer
+    );
 
-  const tx = await semaphore.updateGroupAdmin(GROUP_ID, SEMAPHORE_DEMO_GOERLI_ADDRESS);
+    const tx = await semaphore.updateGroupAdmin(GROUP_ID, SEMAPHORE_DEMO_GOERLI_ADDRESS);
 
-  console.log("Update group admin tx submitted: ", tx.hash);
+    console.log("Update group admin tx submitted: ", tx.hash);
 
-  await tx.wait();
+    await tx.wait();
 
-  console.log("Group admin updated!");
-})
+    console.log("Group admin updated!");
+  })
+}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
